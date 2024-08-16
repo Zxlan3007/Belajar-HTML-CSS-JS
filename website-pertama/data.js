7761e4e712e9a770c7c0ede0906746837a5a7a03
Zxlan3007 <ahmadazlan286@gmail.com>
2024-08-16 14:43:55 +0800

    Hasil belajar

---
+++ website-pertama/data.js
@@ -0,0 +1,28 @@
+// const nama = "ahmad azlan";
+// let usia = 29;
+
+// let biodata = document.getElementById('biodata');
+
+// function generator() {
+//  let golongan;
+ 
+//     if (usia >= 0 && usia < 10) {
+//         golongan = "golongan bayi";
+//     } else if (usia > 10 && usia < 20) {
+//         golongan = "golongan remaja";
+//     } else if (usia > 20 && usia < 30) {
+//         golongan = "golongan dewasa";
+//     } else if (usia >= 30) {
+//         golongan = "golongan tua";
+//     } else {
+//        return console.log('anda tergolong anak yang anh');
+//     }
+
+//     return biodata.innerHTML = golongan;
+
+// }
+
+// console.log(nama);
+// console.log(usia);
+
+// generator()
